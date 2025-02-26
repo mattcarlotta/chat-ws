82a35b69bca9e9cec6c9f4819e86a73dbf909cf3
Matt Carlotta <matt@mattcarlotta.com>
2025-02-26 10:38:29 -0800

    feat: implement auth

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import type { Server, ServerWebSocket } from "bun";
+import jwt from "jsonwebtoken";
+import { createClient } from "@redis/client";
 
 type WebSocketWithData = ServerWebSocket<{
-    clientId: string;
+    userId: string;
     username: string;
 }>;
 
@@ -22,10 +24,14 @@ enum MessageType {
 interface Message {
     type: MessageType;
     username: string;
-    clientId?: string;
+    userId?: string;
     senderId?: string;
     message?: string;
 }
+
+const store = createClient({
+    url: import.meta.env.REDIS_URL,
+});
 
 class WebSocketServer {
     private clients: Map<string, Client> = new Map();
@@ -34,29 +40,66 @@ class WebSocketServer {
     constructor() {
         this.server = Bun.serve({
             port: String(process.env.PORT || 8080),
-            fetch: (req, server): Response | undefined => {
+            fetch: async (req, server): Promise<Response | undefined> => {
+                const username = req.headers.get("username");
+                const password = req.headers.get("password");
                 const url = new URL(req.url);
-                const clientId = url.searchParams.get("clientId");
-                const username = url.searchParams.get("username");
+                const token = url.searchParams.get("token") || "";
 
-                if (!username || !clientId) {
-                    return new Response(
-                        "You must sign in with a username before you can chat!",
-                        {
-                            status: 403,
-                        },
-                    );
+                if (url.pathname === "/" && !token) {
+                    // check if user exists in DB and contains a valid password
+                    if (!username || password !== import.meta.env.PASSWORD) {
+                        return this.sendError(
+                            403,
+                            "You must sign in with a username before you can chat!",
+                        );
+                    }
+
+                    try {
+                        const userId = crypto.randomUUID();
+                        await store.set(userId, username, {
+                            EX: 2592000,
+                        });
+                        const token = jwt.sign({ userId }, import.meta.env.JWT_SECRET);
+
+                        const headers = new Headers();
+                        headers.set(
+                            "Set-Cookie",
+                            `token=${token}; path=/; Max-Age=2592000`,
+                        );
+                        return new Response(`Welcome ${username}.`, {
+                            headers,
+                            status: 201,
+                        });
+                    } catch (error) {
+                        return this.sendError(400, (error as Error)?.message);
+                    }
                 }
 
-                const upgraded = server.upgrade(req, {
-                    data: { clientId, username },
-                });
+                try {
+                    if (!token) {
+                        throw Error("User attempted to connect with an empty token.");
+                    }
+                    const { userId } = jwt.verify(token, import.meta.env.JWT_SECRET, {
+                        maxAge: 2592000,
+                    }) as {
+                        userId: string;
+                    };
 
-                if (upgraded) return;
+                    const username = await store.get(userId || "");
+                    if (!username) {
+                        throw Error("User attempted to connect with an invalid token.");
+                    }
+                    const upgraded = server.upgrade(req, {
+                        data: { userId, username },
+                    });
 
-                return new Response("Upgrade failed - Websocket server only", {
-                    status: 500,
-                });
+                    if (upgraded) return;
+
+                    return this.sendError(500, "Upgrade failed - Websocket server only");
+                } catch (error) {
+                    return this.sendError(403, "Invalid token!");
+                }
             },
             websocket: {
                 maxPayloadLength: 1024 * 1024,
@@ -66,9 +109,7 @@ class WebSocketServer {
             },
         });
 
-        console.log(
-            `WebSocket server running on port: ${this.server.hostname}:${this.server.port}`,
-        );
+        console.log(`WebSocket server running at... ${this.server.url}`);
     }
 
     private broadcast = (data: Message): void => {
@@ -81,13 +122,23 @@ class WebSocketServer {
                     id: crypto.randomUUID(),
                     connectedClients: this.clients.size,
                     message: data.message,
-                    sentByCurrentUser: client.id === data.clientId,
+                    sentByCurrentUser: client.id === data.userId,
                     timestamp: new Date().toISOString(),
                     username: data.username,
                 }),
             );
         }
     };
+
+    private sendError(status: number, err: string): Response {
+        console.error(err);
+        const headers = new Headers();
+        headers.set("Set-Cookie", "token=; path=/; Max-Age=0");
+        return new Response(err, {
+            status,
+            headers,
+        });
+    }
 
     private relayMessage(
         ws: WebSocketWithData,
@@ -110,21 +161,21 @@ class WebSocketServer {
     }
 
     private handleConnection = (ws: WebSocketWithData): void => {
-        const { clientId, username } = ws.data;
+        const { userId, username } = ws.data;
 
-        console.log(`Client connected: ${clientId}`);
+        console.log(`Client connected: ${userId}`);
 
-        if (this.clients.has(clientId)) {
-            console.log(`Client ${clientId} already connected. Reconnecting.`);
+        if (this.clients.has(userId)) {
+            console.log(`Client ${userId} already connected. Reconnecting.`);
         }
 
-        this.clients.set(clientId, { id: clientId, username, socket: ws });
+        this.clients.set(userId, { id: userId, username, socket: ws });
 
         this.relayMessage(ws, { type: MessageType.WELCOME });
 
         this.broadcast({
             type: MessageType.USER_JOINED,
-            senderId: clientId,
+            senderId: userId,
             username,
         });
     };
@@ -147,12 +198,15 @@ class WebSocketServer {
         }
     };
 
-    private handleDisconnection = (ws: WebSocketWithData): void => {
-        const { clientId, username } = ws.data;
+    private handleDisconnection = async (
+        ws: WebSocketWithData,
+    ): Promise<void> => {
+        const { userId, username } = ws.data;
 
-        this.clients.delete(clientId);
+        this.clients.delete(userId);
+        await store.del(userId);
 
-        console.log(`Client diconnected: ${clientId}`);
+        console.log(`Client diconnected: ${userId}`);
 
         this.broadcast({
             type: MessageType.USER_LEFT,
@@ -161,4 +215,17 @@ class WebSocketServer {
     };
 }
 
-new WebSocketServer();
+(async () => {
+    try {
+        await store.connect();
+        console.log("Connected to redis store!");
+
+        new WebSocketServer();
+    } catch (error) {
+        if (store.isOpen) {
+            store.quit();
+        }
+        console.error(error);
+        process.exit(1);
+    }
+})();
